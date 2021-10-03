4ce033f91ea3966ffed4255e6136d80f3a69a269
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-03 18:51:04 -0400

    pseudocode for 33

---
+++ src/main/java/exercise33/solution33.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 33 Solution
+ *  Copyright 2021 Joao Pedro Oliveira
+ */
+
+package exercise33;
+
+import java.util.*;
+
+public class solution33 {
+
+    /*
+    Set a set amount of responses the user can get into a string
+    set a random object
+
+    print "what is your question?"
+    question = user input
+
+    choose a random response from the previously set strings
+
+    print out the response.
+
+     */
+
+}
